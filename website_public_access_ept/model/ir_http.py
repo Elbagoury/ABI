eb50e6d5b4bfad98e3f1bec1da0aca7b134696aa
jeeseon <jeeseonryu@gmail.com>
2018-10-17 16:47:39 +0530

    changes in ir_http file

--- website_public_access_ept/model/ir_http.py
+++ website_public_access_ept/model/ir_http.py
@@ -17,7 +17,7 @@ class IrHttp(models.AbstractModel):
         url=request.httprequest.path
         
         #if not request.session['login'] and (url.find('/web/login')==-1 or url.find('_odoo/paas') ==-1):
-        if not request.session['login'] and url.find('/web/login')==-1:
+        if not request.session['session_token'] and url.find('/web/login')==-1:
             if url.find('/web/')==-1 and url.find('/website/lang/')==-1  and url.find('/website/translations')==-1:
                 _logger.info("\n****** website public access url:_displtch method of ir.http for url %s"%url)
                 redirect = '/web/login'
